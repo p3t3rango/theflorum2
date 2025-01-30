c56cb349e05f94fdde3413282d077b7ab2d66ed1
p3t3rango <pete@peterango.com>
2025-01-29 19:39:14 -0500

    Switch to Next.js runtime configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ['oaidalleapiprodscus.blob.core.windows.net'],
+  },
+  serverRuntimeConfig: {
+    maxDuration: 60,
+  }
 }
 
 module.exports = nextConfig 
